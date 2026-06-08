577cd58f450e824fc4115ed02f8e81cbb1474a87
lizhichao <769306079@qq.com>
2026-06-08 12:10:11 +0800

    chore(build): 更新 IntelliJ IDEA 版本依赖
    - 将 IntelliJ IDEA 依赖从 2026.1 更新到 2026.1.3 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        intellijIdea("2026.1")
+        intellijIdea("2026.1.3")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // Add plugin dependencies for compilation here:
